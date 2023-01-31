25b1b9f1715aed982e4179ba2502129a645a59ff
Ganesh Utla <ganeshutla888@gmail.com>
2023-01-31 07:10:30 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,189 @@
+from flask import Flask, render_template, redirect,request
+import json
+import requests
+import openai
+from docx import Document
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+import re
+import nltk
+import heapq
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("ip.html")   
+
+@app.route("/home")
+def red_home():
+    return redirect("/")   
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    if request.method=='POST':
+        d = {}
+        if request.form["para"]:
+            d["para"] = request.form["para"]
+        else:
+            f = request.files['formFile']
+            ext = (f.filename).split(".")[-1]
+            f.save(f.filename)
+            # print(ext)
+            if ext=="docx":
+                doc = Document(f.filename)
+                paras = doc.paragraphs
+                content = ""
+                for i in range(8,len(paras),3):
+                    p = paras[i+1].text.replace("’","'").replace("‘","'").replace('“','"').replace('”','"').replace("…",".")
+                    content += paras[i].text.split(" - ")[0] + ": " + p + "\n"
+                d["para"] = content
+                
+            elif ext=="txt":
+                tmp = open(f.filename, "r")
+                d["para"] = tmp.read()
+                
+            else:
+                d["para"] = ""
+
+        d["abstractive"] = {}
+        d["extractive"] = {}
+        # print(d)
+
+        # m1 = plaraphy_model(d)
+        # d["abstractive"]["Plaraphy"] = m1
+        # print(m1)
+
+        m2 = openai_model(d)
+        d["abstractive"]["OpenAI"] = m2
+        # print(m2)
+        
+        m3 = lexrank_model(d["para"])
+        d["extractive"]["LexRank"] = m3
+        # print(m3)
+        
+        m4 = latent_summary_analysis_model(d["para"])
+        d["extractive"]["LSA"] = m4
+        # print(m4)
+        
+        m5 = klsum_model(d["para"])
+        d["extractive"]["KL Sum"] = m5
+        # print(m5)
+        
+        m6 = luhn_model(d["para"])
+        d["extractive"]["Luhn"] = m6
+        # print(m6)
+        # m7 = nlp_model(d)
+        # d["abstractive"]["NLP"] = m7
+        # print(d.keys(), d["extractive"].keys(), d["abstractive"].keys())
+
+    return render_template("op.html", data=d)
+
+def plaraphy_model(data):
+    url = 'https://app.plaraphy.com/api/summarizer'
+    payload = 'text='+data["para"]+'&output_percent=10'
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded',
+        'authorization': 'Bearer 24379|3FHJrWgFaALt1iKp3L7MujvMDfHohVNfcOyiQBcT',
+        'cache-control': 'no-cache',
+        }
+    response = requests.request('POST', url, data=payload, headers=headers)
+    # return response.text
+    print(response.text)
+    js = json.loads(response.text)
+    return js["summary"].split(". ")
+
+def openai_model(data):
+    openai.api_key = "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"
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Summarize the following meeting in 10 bullet points: {data}",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    # print(response, type(response))
+    # print(response.choices[0].text, type(response.choices[0].text))
+    # return response.choices[0].text.split('- ' if '-' in response.choices[0].text else '\n\u2022')[1:]
+    return response.choices[0].text.split('\n')[2:]
+
+def lexrank_model(data):
+    from sumy.summarizers.lex_rank import LexRankSummarizer
+    lexrank_summarizer = LexRankSummarizer()
+    sumy_parser = PlaintextParser.from_string(data, Tokenizer('english'))
+    lexrank_summary = lexrank_summarizer(sumy_parser.document,sentences_count=10)
+    # return " ".join(list(map(str, lexrank_summary)))
+    # print(lexrank_summary)
+    return lexrank_summary
+
+def latent_summary_analysis_model(data):
+    from sumy.summarizers.lsa import LsaSummarizer
+    sumy_parser = PlaintextParser.from_string(data, Tokenizer('english'))
+    lsa_summarizer = LsaSummarizer()
+    lsa_summary = lsa_summarizer(sumy_parser.document, 10)
+    # return " ".join(list(map(str, lsa_summary)))
+    return lsa_summary
+
+def luhn_model(data):
+    from sumy.summarizers.luhn import LuhnSummarizer
+    sumy_parser = PlaintextParser.from_string(data, Tokenizer('english'))
+    luhn_summarizer = LuhnSummarizer()
+    luhn_summary = luhn_summarizer(sumy_parser.document, sentences_count=10)
+    # return " ".join(list(map(str,luhn_summary)))
+    return luhn_summary
+
+def klsum_model(data):
+    from sumy.summarizers.kl import KLSummarizer
+    sumy_parser = PlaintextParser.from_string(data, Tokenizer('english'))
+    kl_summarizer = KLSummarizer()
+    kl_summary = kl_summarizer(sumy_parser.document, sentences_count=10)
+    # return " ".join(list(map(str,kl_summary)))
+    return kl_summary
+
+def nlp_model(data):
+
+    article_text = re.sub(r'\[[0-9]*\]', ' ', data)
+    article_text = re.sub(r'\s+', ' ', article_text)
+
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+    sentence_list = nltk.sent_tokenize(article_text)
+    nltk.download("stopwords")
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+                
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+        
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
+
+    summary = ' '.join(summary_sentences)
+    return summary
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ myfile.py
@@ -0,0 +1,106 @@
+# from docx import Document
+
+# doc = Document("./tdoc.docx")
+
+# print(("tdoc.docx").split(".")[-1])
+ 
+# print the list of paragraphs in the document
+# print('List of paragraph objects:->>>')
+# print(doc.paragraphs)
+  
+# print the list of the runs 
+# in a specified paragraph
+# print('\nList of runs objects in 1st paragraph:->>>')
+# print(doc.paragraphs[0].runs)
+  
+# print the text in a paragraph 
+# print('\nText in the 1st paragraph:->>>')
+# print(doc.paragraphs[0].text)
+  
+# for printing the complete document
+# print('\nThe whole content of the document:->>>\n')
+# i = 8
+# paras = doc.paragraphs
+# N = len(paras)
+# while i < N and paras[i].text!="Transcript":
+#     # print(paras[i].text)
+#     i += 1
+# i += 2
+# while i < N:
+#     print(paras[i].text)
+#     i += 1
+
+# res = ""
+# while i < N:
+    # print(pars[i])
+    # print(paras[i].text.split(" - ")[0] + ": " + paras[i+1].text)
+    # res += 
+    # i += 3
+
+s = """Ganesh Utla: Hello. Hello.
+
+Ganesh Utla: Welcome back, Kunal. This is the second part of the interview and I want to get these as quickly as possible because I know you somewhere between. Thank you.
+
+Kunal Agrawal: Thank you. It's my pleasure.
+
+Ganesh Utla: It says on the website you worked doing graphic design for a local branding agency before branching out and starting your own business.
+
+Kunal Agrawal: Yes.
+
+Ganesh Utla: Was that a conscious choice?
+
+Kunal Agrawal: Yes.
+
+Ganesh Utla: Sorry, one sec. We can hear the fan.
+
+Aman Tiwari: [inaudible 00:27]
+
+Ganesh Utla: No, no, That's perfect. Thanks, Aman. Good. Was starting your own company international, or did you just sort of fall into it?
+
+Kunal Agrawal: Actually, Sort of both. I started out doing it as a favour for a friend. I didn't really know what I was doing at the time, but at some point, I found out I was having some success with that, and so I started doing it for local businesses and restaurants. Then it kind of took off from there, and I figured, well, if I'm going to be taking on all these new clients I might as well get a website going and make something out of this, you know?
+
+Ganesh Utla: Sure. What kind of challenges did you experience when you were starting out, that you weren't expecting?
+
+Kunal Agrawal: Hmm, challenges I wasn't expecting.
+
+Ganesh Utla: Hahaha. I keep putting on the spot. I don't think I put that one in the questions either. We're just ad-libbing here.
+
+Kunal Agrawal: No, it's fine. So, challenges.
+
+Ganesh Utla: Okay. I understood. Time to say bye now!
+
+Kunal Agrawal: Okay, once again Thank you for your time.
+
+Ganesh Utla: Same goes for you."""
+
+#MODEL 1
+# import requests
+
+# url = "https://api.meaningcloud.com/summarization-1.0"
+
+# payload={
+#     'key': '26974ec503b66a278e800edbbbfee2f9',
+#     'txt': s,
+#     'sentences': 10
+# }
+
+# response = requests.post(url, data=payload)
+
+# print('Status code:', response.status_code)
+# res = response.json()
+# print(res["summary"])
+
+import requests
+
+url = "https://enelyou-enelyou-summarization--index--summary--topic--part-of-s.p.rapidapi.com/sumpagejson/"
+
+payload = "url=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FNatural_language_processing&length=.1"
+headers = {
+	"content-type": "application/x-www-form-urlencoded",
+	"X-RapidAPI-Key": "80a948574fmsh5676ccf6ef9c454p17718cjsneff8135f3b7d",
+	"X-RapidAPI-Host": "enelyou-enelyou-summarization--index--summary--topic--part-of-s.p.rapidapi.com"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
