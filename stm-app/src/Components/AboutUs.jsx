3ed1d86c6b517e8f18573f8a61df48c490eba327
SultanIndian007 <amantiwari201608@gmail.com>
2023-02-18 18:30:20 +0530

    about-us-changed

--- stm-app/src/Components/AboutUs.jsx
+++ stm-app/src/Components/AboutUs.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>About Our Team Meetings</h2>
-      <p>
-        Our team meetings are a space for us to come together, discuss progress,
-        and plan for the future. We aim to make our meetings productive and
-        efficient, ensuring that everyone has a chance to contribute and be heard.
-      </p>
-
+    <div id="about">
+      <h2>About US</h2>
+      <div id="id">
+        <ul>
+          <li>Our team meetings are a space for us to come together, discuss progress,and plan for the future.</li>
+          <li>We aim to make our meetings productive and efficient, ensuring that everyone has a chance to contribute and be heard</li>
+          <li>At our team meetings app, we understand the importance of effective communication and collaboration within a team.</li>
+          <li>Our app is designed to make team meetings more efficient and productive, allowing teams to focus on what really matters achieving their goals.</li>
+          <li>Our team meetings app provides a range of features that enable team members to prepare for meetings, share their ideas, and collaborate in real-time.</li>
+          <li> With our app, you can create and schedule meetings, set agendas, assign tasks, and share documents and files, all from one central location.</li>
+          <li>Our app is designed to be user-friendly and intuitive, with a clean and simple interface that makes it easy to navigate.</li>
+          <li>It's perfect for teams of all sizes, whether you're a small startup or a large enterprise</li>
+          <li>We're committed to providing the best possible experience for our users, and we're constantly working to improve our app based on your feedback.</li>
+          <li>Our team is dedicated to ensuring that our app meets the needs of your team, and we're always here to help if you have any questions or concerns.</li>
+          <li>In short, our team meetings app is the perfect solution for teams looking to streamline their meetings and improve their collaboration.</li>
+          <li>We believe that by using our app, teams can be more productive, efficient, and successful in achieving their goals.</li>
+        </ul>
+        </div>
     </div>
   );
 };
